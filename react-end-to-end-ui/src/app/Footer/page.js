d6d3a1ada2888f3f36d85678bd9022b36b1655c6
klakshmaiah <kollilakshmaiah1912@gmail.com>
2024-07-03 09:36:30 +0530

    adding header and footer

---
+++ react-end-to-end-ui/src/app/Footer/page.js
@@ -0,0 +1,6 @@
+import React from "react";
+import footer from './footer.page.module.css'
+  const Footer=()=>{
+   return <div className={footer.footer}>Footer</div>
+}
+export default Footer;
---
+++ react-end-to-end-ui/src/app/Header/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import headerCss from './header.module.css'
+export const Header=()=>{
+    return <div className={headerCss.header}>
+      Header
+    </div>
+}
+export default Header;
